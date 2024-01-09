f89ffb6571a9f4cb530f456020f49627b0175658
Vineet Naik <naikvin@gmail.com>
2024-01-09 10:50:11 +0530

    Add 'early-init' file to disable the title bar in emacs
    More info:
    https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html

---
+++ emacs/.emacs.d/early-init.el
@@ -0,0 +1,7 @@
+;; This file is loaded before the package system and GUI is
+;; initialized, so in it you can customize variables that affect the
+;; package initialization process
+;;
+;; For more info refer: https://www.gnu.org/software/emacs/manual/html_node/emacs/Early-Init-File.html
+
+(add-to-list 'default-frame-alist '(undecorated . t))
